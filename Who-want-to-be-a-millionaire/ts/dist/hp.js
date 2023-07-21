040398ba273861df110915dfd02ad9eb61a275ab
Lee_D <leegreat21@gmail.com>
2023-07-21 14:41:16 +0300

    home-page

---
+++ Who-want-to-be-a-millionaire/ts/dist/hp.js
@@ -0,0 +1,8 @@
+function playSound() {
+    console.log("Trying to play audio...");
+    var audio = new Audio('./ts/dist/intro.mp3');
+    audio.play()
+        .then(function () { return console.log("Audio played successfully!"); })["catch"](function (error) { return console.error("Error playing audio:", error); });
+}
+var playButton = document.getElementById('play');
+playButton.addEventListener("click", playSound);
