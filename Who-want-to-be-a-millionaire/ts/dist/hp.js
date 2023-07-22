a526640a70382c051cb35c2280f9472361c82c77
Lee_D <leegreat21@gmail.com>
2023-07-22 11:11:41 +0300

    music-fix

--- Who-want-to-be-a-millionaire/ts/dist/hp.js
+++ Who-want-to-be-a-millionaire/ts/dist/hp.js
@@ -1,8 +1,6 @@
 function playSound() {
-    console.log("Trying to play audio...");
-    var audio = new Audio('./ts/dist/intro.mp3');
-    audio.play()
-        .then(function () { return console.log("Audio played successfully!"); })["catch"](function (error) { return console.error("Error playing audio:", error); });
+    var audio = new Audio('/Who-want-to-be-a-millionaire/audio/into.mp3');
+    audio.play();
 }
 var playButton = document.getElementById('play');
 playButton.addEventListener("click", playSound);
