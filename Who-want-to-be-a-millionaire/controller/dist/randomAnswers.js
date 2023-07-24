0cc52424cac9b0aa76e62fbd12b991c5735824cb
ziv leloneck <119840698+zivll1991@users.noreply.github.com>
2023-07-24 16:27:54 +0300

    shufflesAnswers function
    This function takes four answers, shuffles them, and returns them in a different order

---
+++ Who-want-to-be-a-millionaire/controller/dist/randomAnswers.js
@@ -0,0 +1,43 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+// This function takes four answers, shuffles them, and returns them in a different order
+function shufflesAnswers(answers) {
+    try {
+        var shuffledAnswers = [];
+        var newAnswer = __spreadArrays(answers);
+        debugger;
+        for (var i = 0; i < answers.length; i++) {
+            var tempAnswer = Math.floor(Math.random() * answers.length);
+            if (newAnswer[tempAnswer] !== undefined) {
+                shuffledAnswers.push(newAnswer[tempAnswer]);
+                newAnswer.splice(tempAnswer, 1);
+            }
+            else {
+                i--;
+            }
+        }
+        return shuffledAnswers;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+// interface Props {
+//   answer: number;
+//   // answer2: number;
+//   // answer3: number;
+//   // answer4: number;
+// }
+// const numbers: Props[] = [
+//   { answer: 1 },
+//   { answer: 2 },
+//   { answer: 3 },
+//   { answer: 4 },
+// ];
+// // Answers
+// console.log(shufflesAnswers(numbers));
