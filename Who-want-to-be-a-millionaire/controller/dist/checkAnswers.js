03598cb0ee3e0697f1501531e04c52dadeb75622
Lee_D <leegreat21@gmail.com>
2023-07-25 13:38:29 +0300

    pseudo-code

--- Who-want-to-be-a-millionaire/controller/dist/checkAnswers.js
+++ Who-want-to-be-a-millionaire/controller/dist/checkAnswers.js
@@ -1,1 +1,5 @@
-// import {checkAnswers} from './dist/interface.js'
+//checking answers - פונקציה שבודקת אם המשתמש ענה נכון או לא  
+// handle result - היא מופעלת על פי מה שהתקבל בפונקציה למעלה  תנאי - TRUE|FALSE  פונקציה
+//-אם התשובה לא נכונה הפונקציה תפנה ל handle lose - המשחק נגמר 
+// אם התשובה נכונה - הפונקציה מפנה ל- habdle win והשחקן עולה שלב
+// correct answer - עולה שלב בסכום שנצבר + בסולם
