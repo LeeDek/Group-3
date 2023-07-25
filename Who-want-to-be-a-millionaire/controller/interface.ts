1fb0f3a6512cdd5958ef4407c3c9eab7fc65a7d2
Lee_D <leegreat21@gmail.com>
2023-07-25 13:37:20 +0300

    questionsArray

--- Who-want-to-be-a-millionaire/controller/interface.ts
+++ Who-want-to-be-a-millionaire/controller/interface.ts
@@ -14,7 +14,7 @@ const player1: User = {
     userName: "LeeDee",
     password: "Oscar123",
     uid: generateUid(),
-   
+
 }
 
 interface Question {
@@ -27,98 +27,100 @@ interface Question {
     correctAnswer: string;
 }
 
-const question1: Question = {
-    uid:  generateUid(),
-    questionText: "What is the capital city of Colombia?",
-    answerA: "Buenos Aires",
-    answerB: "Lima",
-    answerC: "Sao Paolo",
-    answerD: "Bogota",
-    correctAnswer: "Bogota",
-}
+const questionsArray: Question = [
 
-const question2: Question = {
-    uid:  generateUid(),
-    questionText: "In the medical profession, what do the initials 'GP' stand for?",
-    answerA: "Good Practitioner",
-    answerB: "Garden Practitioner",
-    answerC: "General Practitioner",
-    answerD: "Graded Practitioner",
-    correctAnswer: "General Practitioner",
-}
+    {
+        uid: generateUid(),
+        questionText: "What is the capital city of Colombia?",
+        answerA: "Buenos Aires",
+        answerB: "Lima",
+        answerC: "Sao Paolo",
+        answerD: "Bogota",
+        correctAnswer: "Bogota",
+    },
 
-const question3: Question = {
-    uid:  generateUid(),
-    questionText: "What is the smallest country in the world?",
-    answerA: "Seychelles",
-    answerB: "Iceland",
-    answerC: "Spain",
-    answerD: "Vatican City",
-    correctAnswer: "Vatican City",
-}
+{
+        uid: generateUid(),
+        questionText: "In the medical profession, what do the initials 'GP' stand for?",
+        answerA: "Good Practitioner",
+        answerB: "Garden Practitioner",
+        answerC: "General Practitioner",
+        answerD: "Graded Practitioner",
+        correctAnswer: "General Practitioner",
+    },
 
-const question4: Question = {
-    uid:  generateUid(),
+ {
+        uid: generateUid(),
+        questionText: "What is the smallest country in the world?",
+        answerA: "Seychelles",
+        answerB: "Iceland",
+        answerC: "Spain",
+        answerD: "Vatican City",
+        correctAnswer: "Vatican City",
+    },
+
+ {
+    uid: generateUid(),
     questionText: "In what year was the first iPhone released?",
     answerA: "2009",
     answerB: "2004",
     answerC: "2007",
     answerD: "2000",
     correctAnswer: "2007",
-}
+},
 
-const question5: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "How many million miles away is the sun from the Earth?",
     answerA: "93 million",
     answerB: "12 million",
     answerC: "136 million",
     answerD: "200 million",
     correctAnswer: "93 million",
-}
+},
 
-const question6: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "Which city will host the 2028 olympic games?",
     answerA: "Rome",
     answerB: "Oslo",
     answerC: "Paris",
     answerD: "Los Angeles",
     correctAnswer: "Los Angeles",
-}
+},
 
-const question7: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "What is the name of the famous tower in Italy that leans to one side?",
     answerA: "Eiffel Tower",
     answerB: "CN Tower",
     answerC: "Leaning Tower of Pisa",
     answerD: "Tokyo Tower",
     correctAnswer: "Leaning Tower of Pisa",
-}
+},
 
-const question8: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "What is the smallest continent in the world?",
     answerA: "Antartica",
     answerB: "Africa",
     answerC: "Australia",
     answerD: "South America",
     correctAnswer: "Australia",
-}
+},
 
-const question9: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "What is the tallest animal in the world?",
     answerA: "Hippopotamus",
     answerB: "Giraffe",
     answerC: "Rhine",
     answerD: "Elephant",
     correctAnswer: "Giraffe",
-}
+},
 
-const question10: Question = {
-    uid:  generateUid(),
+{
+    uid: generateUid(),
     questionText: "What is the largest continent in the world?",
     answerA: "Africa",
     answerB: "Asia",
@@ -126,7 +128,7 @@ const question10: Question = {
     answerD: "South America",
     correctAnswer: "Asia",
 }
-
+];
 
 
 
