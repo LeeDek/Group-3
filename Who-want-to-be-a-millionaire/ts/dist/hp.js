775524cf0677dd28f3518a8841bc936b7e391178
Lee_D <leegreat21@gmail.com>
2023-07-29 11:19:25 +0300

    game-over-and-sounds

--- Who-want-to-be-a-millionaire/ts/dist/hp.js
+++ Who-want-to-be-a-millionaire/ts/dist/hp.js
@@ -1,16 +1,17 @@
-var audioPlayed = false;
-function playSound() {
-    if (!audioPlayed) {
-        console.log("Trying to play audio...");
-        var audio = new Audio('/Who-want-to-be-a-millionaire/audio/intro.mp3');
-        audio.play()
-            .then(function () {
-            console.log("Audio played successfully!");
-            audioPlayed = true;
-            var bodyElement = document.querySelector('body');
-            bodyElement.removeEventListener("mouseenter", playSound);
-        })["catch"](function (error) { return console.error("Error playing audio:", error); });
-    }
-}
-var bodyElement = document.querySelector('body');
-bodyElement.addEventListener("mouseenter", playSound);
+// let audioPlayed: boolean = false; 
+// function playSound() {
+//   if (!audioPlayed) {
+//     console.log("Trying to play audio...");
+//     const audio = new Audio('/Who-want-to-be-a-millionaire/audio/intro.mp3');
+//     audio.play()
+//       .then(() => {
+//         console.log("Audio played successfully!");
+//         audioPlayed = true; 
+//         const bodyElement: HTMLBodyElement | null = document.querySelector('body');
+//         bodyElement.removeEventListener("mouseenter", playSound); 
+//       })
+//       .catch(error => console.error("Error playing audio:", error));
+//   }
+// }
+// const bodyElement = document.querySelector('body') as HTMLElement;
+// bodyElement.addEventListener("mouseenter", playSound);
--- Who-want-to-be-a-millionaire/ts/hp.ts
+++ Who-want-to-be-a-millionaire/ts/hp.ts
@@ -1,21 +1,21 @@
-let audioPlayed: boolean = false; 
+// let audioPlayed: boolean = false; 
 
-function playSound() {
-  if (!audioPlayed) {
-    console.log("Trying to play audio...");
-    const audio = new Audio('/Who-want-to-be-a-millionaire/audio/intro.mp3');
-    audio.play()
-      .then(() => {
-        console.log("Audio played successfully!");
-        audioPlayed = true; 
-        const bodyElement: HTMLBodyElement | null = document.querySelector('body');
-        bodyElement.removeEventListener("mouseenter", playSound); 
-      })
+// function playSound() {
+//   if (!audioPlayed) {
+//     console.log("Trying to play audio...");
+//     const audio = new Audio('/Who-want-to-be-a-millionaire/audio/intro.mp3');
+//     audio.play()
+//       .then(() => {
+//         console.log("Audio played successfully!");
+//         audioPlayed = true; 
+//         const bodyElement: HTMLBodyElement | null = document.querySelector('body');
+//         bodyElement.removeEventListener("mouseenter", playSound); 
+//       })
 
-      .catch(error => console.error("Error playing audio:", error));
-  }
-}
+//       .catch(error => console.error("Error playing audio:", error));
+//   }
+// }
 
 
-const bodyElement = document.querySelector('body') as HTMLElement;
-bodyElement.addEventListener("mouseenter", playSound);
+// const bodyElement = document.querySelector('body') as HTMLElement;
+// bodyElement.addEventListener("mouseenter", playSound);
