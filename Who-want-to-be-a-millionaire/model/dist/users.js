dfe5b99ec87ece485e54e9c765bfd8d8baa5577a
navered <128045047+navered@users.noreply.github.com>
2023-07-31 17:34:20 +0300

    new register

--- Who-want-to-be-a-millionaire/model/dist/users.js
+++ Who-want-to-be-a-millionaire/model/dist/users.js
@@ -1,1 +1,9 @@
+var User = /** @class */ (function () {
+    function User(userName, password, uid) {
+        this.userName = userName;
+        this.password = password;
+        this.uid = uid;
+    }
+    return User;
+}());
 var users = [];
