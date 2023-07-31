6807757878762cd918dbbc9b60c99813c1ddeeb7
Lee_D <leegreat21@gmail.com>
2023-07-31 15:59:43 +0300

    loadNextQuestion

---
+++ Who-want-to-be-a-millionaire/controller/dist/loadNextQuestion.js
@@ -0,0 +1,20 @@
+function loadNextQuestion() {
+    try {
+        var stage = 0;
+        if (stage < questionsArray.length - 1) {
+            var currentQuestion = questionsArray[stage];
+            document.querySelector(".question__text h2").textContent = currentQuestion.questionText;
+            document.querySelector(".a").textContent = currentQuestion.answerA;
+            document.querySelector(".b").textContent = currentQuestion.answerB;
+            document.querySelector(".c").textContent = currentQuestion.answerC;
+            document.querySelector(".d").textContent = currentQuestion.answerD;
+            stage++;
+        }
+        else {
+            endGame();
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
